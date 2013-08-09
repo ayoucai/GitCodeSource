3b00524099c548fff1ecb35f41786d16b4311bbb
ayoucai <952081338@qq.com>
2013-08-09 10:47:54 +0800

    Test

---
+++ hangman.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#define DICT "words.txt"
+#define EDICT "/crp/dict/web2"
+#define MAXERR 7
+#define 
